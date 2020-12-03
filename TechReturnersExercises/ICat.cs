0d31f1b8b2a30d88f19e1acff05088d9314a011c
Jason Morris <37531289+JasonM0rris@users.noreply.github.com>
2020-12-03 21:27:48 +0000

    Update ICat.cs

--- TechReturnersExercises/ICat.cs
+++ TechReturnersExercises/ICat.cs
@@ -1,79 +1,20 @@
+  
 using System;
 
 namespace TechReturners.Exercises
 {
-    // implements ICat interface
-    public abstract class Cat : ICat
+    public interface ICat
     {
-        //properties
-        public bool IsAsleep { get; private set; }
-        public string Setting { get; private set; }
-        public int AverageHeight { get; private set; }
-        public virtual string Eat { get; private set; }
-
-
-        // constructor
-        public Cat(string setting, int averageHeight, string eat)
-        {
-            this.IsAsleep = false;
-            this.Setting = setting;
-            this.AverageHeight = averageHeight;
-            this.Eat = eat;
-        }
+        // properties
+        bool IsAsleep { get; }
+        string Setting { get; }
+        int AverageHeight { get; }
+        string Eat { get; }
 
         // methods
-        public void GoToSleep()
-        {
-            this.IsAsleep = true;
-        }
+        void GoToSleep();
+        void WakeUp();
 
-        public void WakeUp()
-        {
-            this.IsAsleep = false;
-        }
     }
 
-    // derived classes
-    public class DomesticCat : Cat
-    {
-
-        Random random = new Random();
-
-        // use base class constructor
-        public DomesticCat() : base("domestic", 23, "Purrrrrrr")
-        {
-        }
-
-        private string randomResponse()
-        {
-            int num = random.Next(10);
-            if (num < 5)
-            {
-                return "It will do I suppose";
-            }
-            else
-            {
-                return "Purrrrrrr";
-            }
-        }
-        // polymorphism.. overriding the getter
-        public override string Eat
-        {
-            get => randomResponse();
-        }
-    }
-
-    public class LionCat : Cat
-    {
-        public LionCat() : base("lion", 1100, "Roar!!!!")
-        {
-        }
-    }
-
-    public class CheetahCat : Cat
-    {
-        public CheetahCat() : base("cheetah", 900, "Zzzzzzz")
-        {
-        }
-    }
 }
